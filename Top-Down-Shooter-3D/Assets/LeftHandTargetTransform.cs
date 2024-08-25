f6c123cd5c8841b914dc672a6bf5e4c3b478e31f
Rishabh Garg <narishabh.garg@gmail.com>
2024-08-25 20:57:24 +0530

    Implemented Left Hand IKs for different weapons

---
+++ Top-Down-Shooter-3D/Assets/LeftHandTargetTransform.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftHandTargetTransform : MonoBehaviour
+{
+    // This script is created to fetch left hand position and rotation based on weapon selected
+}
